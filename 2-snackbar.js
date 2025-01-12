365373d4ff41d988a2a9e660485ef83aa0bb3c50
ankhod <120341777+ankhod@users.noreply.github.com>
2025-01-12 17:49:41 +0000

    Deploying to gh-pages from @ ankhod/goit-js-hw-10@a0edec2a5d0bdeb6468cef72b14df6e04a7b35e7 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i}from"./assets/vendor-A92OCY9B.js";document.querySelector(".form").addEventListener("submit",function(s){s.preventDefault();const t=parseInt(s.target.delay.value,10),r=s.target.state.value;new Promise((e,o)=>{setTimeout(()=>{r==="fulfilled"?e(t):o(t)},t)}).then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})});i.success({title:"Test",message:"iziToast is working!"});
+//# sourceMappingURL=2-snackbar.js.map
